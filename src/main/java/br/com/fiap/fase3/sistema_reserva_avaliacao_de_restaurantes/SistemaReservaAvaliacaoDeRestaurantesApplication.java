d7422f00b73ab3006710abb66abdcbc874f4dd92
andersonsilva8609 <andersonsilva8609@gmail.com>
2024-10-01 00:09:45 -0300

    primeiro commit

---
+++ src/main/java/br/com/fiap/fase3/sistema_reserva_avaliacao_de_restaurantes/SistemaReservaAvaliacaoDeRestaurantesApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fase3.sistema_reserva_avaliacao_de_restaurantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaReservaAvaliacaoDeRestaurantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaReservaAvaliacaoDeRestaurantesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/fase3/sistema_reserva_avaliacao_de_restaurantes/SistemaReservaAvaliacaoDeRestaurantesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fase3.sistema_reserva_avaliacao_de_restaurantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaReservaAvaliacaoDeRestaurantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
